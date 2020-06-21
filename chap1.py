383b2210f538fa12adc47dd42a4f7dbda88ff0a8
himaeda1332 <himaeda@umich.edu>
2020-06-20 21:23:23 -0400

    Add deep-learning 2 codes

---
+++ chap1.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class Sigmoid:
+    def __init__(self):
+        self.params = []
+        self.out = None
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+
+
+
+
+
+
+
+x = np.random.randn(10, 2)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+
